fc6fa9c5ce55dcb7f64d5c33d7de947caf40e584
James Lin <james.lin1997@hotmail.com>
2018-05-03 11:08:25 +1000

    basic level select + game functionality (incomplete)

--- src/gridlock/controllers/MenuController.java
+++ src/gridlock/controllers/MenuController.java
@@ -6,14 +6,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class MenuController {
     @FXML
     private ToggleGroup difficulty;
     @FXML
     private ToggleGroup gameMode;
+    @FXML
+    private ToggleGroup level;
+
+//    private void getDifficulty() {
+//        System.out.println(difficulty.getSelectedToggle());
+//    }
+//
+//    private void getGameMode() {
+//        System.out.println(gameMode.getSelectedToggle());
+//    }
 
     @FXML
     private void navToMenu(ActionEvent event) throws Exception {
@@ -25,12 +39,50 @@ public class MenuController {
 
     @FXML
     private void navToPlaySettings(ActionEvent event) throws Exception {
-        System.out.print("HIEIHEIH");
         Parent playSettingsParent = FXMLLoader.load(getClass().getResource("../templates/PlaySettings.fxml"));
-        System.out.print("asdsadsads");
         Scene playSettingsScene = new Scene(playSettingsParent);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(playSettingsScene);
+    }
+
+    @FXML
+    private void playControl(ActionEvent event) throws Exception {
+        // TODO: Modify ToggleButton so that something is always selected. (So try/catch is not needed)
+        try {
+            ToggleButton selectedDifficulty = (ToggleButton) difficulty.getSelectedToggle();
+            String selectedDifficultyValue = selectedDifficulty.getText();
+
+            ToggleButton selectedGameMode = (ToggleButton) gameMode.getSelectedToggle();
+            String selectedGameModeValue = selectedGameMode.getText();
+
+            if (selectedGameModeValue.equals("Campaign")) {
+                // Load Game Menu
+                navToLevelSelect(event, selectedDifficultyValue);
+            }
+            else {
+                assert selectedGameModeValue.equals("Sandbox");
+                navToGame(event, selectedDifficultyValue);
+            }
+        }
+        catch (NullPointerException e) {
+            System.out.println("Please specify a difficulty and game mode.");
+        }
+    }
+
+    // TODO: Handle Mode
+    private void navToLevelSelect(ActionEvent event, String difficultyMode) throws Exception {
+        Parent levelSelectParent = FXMLLoader.load(getClass().getResource("../templates/LevelSelect.fxml"));
+        Scene levelSelectScene = new Scene(levelSelectParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(levelSelectScene);
+    }
+
+    // TODO: Handle Mode
+    private void navToGame(ActionEvent event, String difficultyMode) throws Exception {
+        Parent gameParent = FXMLLoader.load(getClass().getResource("../templates/Game.fxml"));
+        Scene gameScene = new Scene(gameParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(gameScene);
     }
 
     @FXML
