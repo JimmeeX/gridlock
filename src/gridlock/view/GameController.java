176d733fa18e82f65b6b35ed799a421d067b2d80
JimmeeX <james.lin1997@hotmail.com>
2018-05-06 13:11:01 +1000

    small changes to design

--- src/gridlock/view/GameController.java
+++ src/gridlock/view/GameController.java
@@ -6,12 +6,18 @@ import gridlock.model.Mode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class GameController {
     private Board board;
@@ -42,6 +48,30 @@ public class GameController {
         this.board.printGrid();
 
         // TODO: Draw Board Here
+//        int num = 36;
+//        int maxColumns = 6;
+//        int maxRows = 6;
+//        GridPane boardGame = new GridPane();
+//        boardGame.setAlignment(Pos.CENTER);
+//        Collection<StackPane> stackPanes = new ArrayList<StackPane>();
+//        for (int row = 0; row < maxRows; row++) {
+//            for (int col = maxColumns - 1; col >= 0; col--) {
+//                StackPane stackPane = new StackPane();
+//
+//                // To occupy fixed space set the max and min size of
+//                // stackpanes.
+//                // stackPane.setPrefSize(150.0, 200.0);
+//                stackPane.setMaxSize(100.0, 100.0);
+//                stackPane.setMinSize(100.0, 100.0);
+//
+//                boardGame.add(stackPane, col, row);
+//                stackPanes.add(stackPane);
+//                num--;
+//            }
+//        }
+//        boardGame.setGridLinesVisible(true);
+//        boardGame.autosize();
+//
     }
 
     @FXML
@@ -51,4 +81,5 @@ public class GameController {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(menuScene);
     }
+
 }
