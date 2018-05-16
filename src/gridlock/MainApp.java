4b6afa66447d97e984238f3d1eee5d7bbe218d63
JimmeeX <james.lin1997@hotmail.com>
2018-05-16 22:12:17 +1000

    added handle to closing application via 'x'

--- src/gridlock/MainApp.java
+++ src/gridlock/MainApp.java
@@ -8,41 +8,52 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 
 public class MainApp extends Application {
+    private SystemSettings settings;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("view/Menu.fxml"));
         Parent menuParent = loader.load();
         Scene menuScene = new Scene(menuParent);
 
-        SystemSettings settings;
         // Initialise Settings
         // Try Reading from Serialized Data
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("src/gridlock/resources/save.data")))) {
-            settings = (SystemSettings) ois.readObject();
-            settings.initSounds(1.0, 1.0);
+            this.settings = (SystemSettings) ois.readObject();
+            this.settings.initSounds(1.0, 1.0);
             System.out.println("Data successfully loaded.");
         }
 
         // If Reading Failed, Create new File
         catch (IOException e) {
             System.out.println("No save file found. Creating new File.");
-            settings = new SystemSettings(1.0,1.0);
+            this.settings = new SystemSettings(1.0,1.0);
         }
 
         MenuController menuController = loader.getController();
-        menuController.initData(settings);
+        menuController.initData(this.settings);
+
+        primaryStage.setOnCloseRequest(e -> {
+            try {closeProgram(primaryStage);}
+            catch (Exception exception) {System.out.println("Saving data failed");}
+        });
 
         primaryStage.setTitle("Gridlock");
         primaryStage.setScene(menuScene);
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    private void closeProgram(Stage stage) throws Exception {
+        // Save Data
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("src/gridlock/resources/save.data")))) {
+            oos.writeObject(this.settings);
+        }
+        stage.close();
     }
 
     public static void main(String[] args) {
